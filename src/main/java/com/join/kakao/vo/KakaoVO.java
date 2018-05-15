205ae90ed947c604933d6721e270a06bb2aed2a7
datascish <datascish@gmail.com>
2018-05-15 13:46:00 +0900

    ...

--- src/main/java/com/join/kakao/vo/KakaoVO.java
+++ src/main/java/com/join/kakao/vo/KakaoVO.java
@@ -1,5 +1,26 @@
 package com.join.kakao.vo;
 
+import java.util.Map;
+
 public class KakaoVO {
+	private String id;
+	private Map<String, String> properties;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public Map<String, String> getProperties() {
+		return properties;
+	}
+	
+	public void setProperties(Map<String, String> properties) {
+		this.properties = properties;
+	}
+	
 	
 }
