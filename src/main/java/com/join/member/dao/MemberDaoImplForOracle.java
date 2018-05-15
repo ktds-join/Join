cabd0b6de7d9702ebcbe5e6e0d8ec0b92f560bb9
Wonjun Choi <cwj0114@gmail.com>
2018-05-15 11:27:00 +0900

    login 1st modification

--- src/main/java/com/join/member/dao/MemberDaoImplForOracle.java
+++ src/main/java/com/join/member/dao/MemberDaoImplForOracle.java
@@ -12,23 +12,8 @@ public class MemberDaoImplForOracle extends SqlSessionDaoSupport implements Memb
 	}
 	
 	@Override
-	public int selectCountMemberEmail(String email) {
-		return getSqlSession().selectOne("MemberDao.selectCountMemberEmail", email);
-	}
-	
-	@Override
-	public int selectCountMemberNickname(String nickname) {
-		return getSqlSession().selectOne("MemberDao.selectCountMemberNickname", nickname);
-	}
-	
-	@Override
 	public int insertMember(MemberVO memberVO) {
 		return getSqlSession().insert("MemberDao.insertMember",memberVO);
-	}
-
-	@Override
-	public int deleteMember(int userId) {
-		return getSqlSession().delete("MemberDao.deleteMember", userId);
 	}
 
 }
