2433d8a8142fe3b6d314a57145efd70a347b067d
LAPTOP-B1TL97A8\lucy <juyeon.k2m@gmail.com>
2018-05-16 16:26:16 +0900

    주연 - MainController의 import 부분에 수정

--- src/main/java/com/join/main/web/MainController.java
+++ src/main/java/com/join/main/web/MainController.java
@@ -5,11 +5,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.join.main.service.MainService;
-import com.join.member.constants.Member;
-import com.join.main.vo.StyleVO;
 
 @Controller
 public class MainController {
